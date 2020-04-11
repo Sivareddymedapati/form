5bf60992948d4577f7357821d4257e8811e1bf9d
Sivareddymedapati <sivareddymedapati@gmail.com>
2020-04-11 21:40:01 +0530

    Updates

---
+++ precache-manifest.0e0efa978ec89229f1cecc029ae0b6b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20416404cd908d0b2ced9860e1928e2f",
+    "url": "/form/index.html"
+  },
+  {
+    "revision": "a66d74c7bb6340eb04f3",
+    "url": "/form/static/css/main.77515b1f.chunk.css"
+  },
+  {
+    "revision": "8b5294a3306a0c0204bc",
+    "url": "/form/static/js/2.424be6f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/form/static/js/2.424be6f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a66d74c7bb6340eb04f3",
+    "url": "/form/static/js/main.c93e59f8.chunk.js"
+  },
+  {
+    "revision": "781f6bfd6aeb8014ddcb",
+    "url": "/form/static/js/runtime-main.a194e0af.js"
+  }
+]);
